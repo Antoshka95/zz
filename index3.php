4f503520483c022b2bbcf5b3805a8536531171cf
Antoshka95 <TonyaGold@yandex.ru>
2017-10-03 14:55:39 +0700

    Проверка на существование элемента

---
+++ index3.php
@@ -0,0 +1,15 @@
+<?php
+$prices=[120, 130, 150, 130, 100];
+$prices[0]=50;
+$prices[]=200;
+$prices[100]=150;
+$summ=0;
+
+for ($i=0; $i<count($prices); $i++) {
+	if (isset($prices[$i])) {
+		$summ+=$prices[$i];
+	}
+}
+$avg=$summ/count($prices);
+echo "Среднее арифметическое $avg";
+?>
